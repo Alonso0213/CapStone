6cd5c208da9d0f48d256135cd188ac20a87eec7f
Alonso0213 <alonsocupido51@gmail.com>
2023-09-04 11:50:57 +0200

    admin table inserted

--- front-end/src/views/AdminView.vue
+++ front-end/src/views/AdminView.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-
+<navComp/>
+<h2>Admin</h2>
+<adminComp/>
     </div>
 </template>
 
 <script>
+import navComp from '../components/navComp.vue'
+import adminComp from '../components/adminComp.vue'
+
     export default {
-        
+        components:{
+            navComp,
+            adminComp
+        }
     }
 </script>
 
