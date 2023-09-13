970737b8bfc07fc4031d3bf0a1f7171b65f15b6c
Alonso0213 <alonsocupido51@gmail.com>
2023-09-13 11:24:04 +0200

    update

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -3,74 +3,78 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
   },
   {
-    path: '/contact',
-    name: 'contact',
-    component: () => import('../views/ContactView.vue')
+    path: "/contact",
+    name: "contact",
+    component: () => import("../views/ContactView.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/LoginView.vue')
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginView.vue"),
   },
   {
-    path: '/register',
-    name: 'register',
-    component: () => import('../views/RegisterView.vue')
+    path: "/register",
+    name: "register",
+    component: () => import("../views/RegisterView.vue"),
   },
   {
-    path: '/parking',
-    name: 'parking',
-    component: () => import('../views/ParkingView.vue')
+    path: "/parking",
+    name: "parking",
+    component: () => import("../views/ParkingView.vue"),
   },
   {
-    path: '/checkout',
-    name: 'checkout',
-    component: () => import('../views/CheckoutView.vue')
+    path: "/checkout",
+    name: "checkout",
+    component: () => import("../views/CheckoutView.vue"),
   },
   {
-    path: '/admin',
-    name: 'admin',
-    component: () => import('../views/AdminView.vue')
+    path: "/admin",
+    name: "admin",
+    component: () => import("../views/AdminView.vue"),
   },
   {
-    path: '/single/:id?',
-    name: 'singlePage',
-    component: () => import('../views/singleView.vue')
+    path: "/singleUser",
+    name: "S_user",
+    component: () => import("../views/SingleUserview.vue"),
+  },
+  {
+    path: "/single/:id?",
+    name: "singlePage",
+    component: () => import("../views/singleView.vue"),
   },
   {
     path: "/addparking",
     name: "addparking",
-    component: () => import("../views/AddView.vue")
+    component: () => import("../views/AddView.vue"),
   },
   {
     path: "/adduser",
     name: "adduser",
-    component: () => import("../views/addUserView.vue")
+    component: () => import("../views/addUserView.vue"),
   },
   {
     path: "/editparking/:id",
     name: "editparking",
     props: true,
-    component: () => import("../views/editView.vue")
+    component: () => import("../views/editView.vue"),
   },
   {
     path: "/edituser/:id",
     name: "edituser",
     component: () => import("../views/editUserView.vue"),
-    props: true
-  }
-
-]
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
